38a4186287028f587045fba193aaa29a16a26a94
Matthew Burns <msb@ameritech.net>
2023-03-17 11:49:57 -0400

    Refactored newtweet

--- app/javascript/components/Home/Feed/FeedNewTweet.jsx
+++ app/javascript/components/Home/Feed/FeedNewTweet.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import ProfilePicSm from '../../shared/ProfilePicSm'
-import TweetRight from './TweetRight'
 
 function FeedNewTweet() {
   return (
     <div id='FeedNewTweet' className='Tweet'>
       <ProfilePicSm />
-      <TweetRight isNewTweet={true} />
+      
+      <div className='TweetRight' id='newTweetRight'>
+        <div className='TweetContent'>
+          <input className='TweetContent__input' type='text' placeholder="What's happening?" />
+        </div>
+
+        <div className='TweetActions'>
+          <div className='TweetActions__left'>[Placeholder for media]</div>
+          <div className='TweetActions__right'>
+            <button className='TweetActions__right__post'>Tweet</button>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
--- app/javascript/components/Home/Feed/TweetRight.jsx
+++ app/javascript/components/Home/Feed/TweetRight.jsx
@@ -1,78 +1,23 @@
 import React from 'react'
-import TweetAuthorInfo from './TweetAuthorInfo'
 
 function TweetRight({ isNewTweet, tweetObject }) {
 
-  // Setup ID for TweetRight div
-  const tweetElID = (() => {
-    if (isNewTweet) return 'newTweet'
-    return `tweet-${tweetObject.id}`
-  })()
-
-  // If tweet is new, add the newTweet class to the TweetRight__authorInfo div
-  let tweetAuthorInfoClass = 'TweetRight__authorInfo'
-  if (isNewTweet) {
-    tweetAuthorInfoClass += ' newTweet'
-  }
-
-  // If tweet is new, render a blank TweetAuthorInfo div
-  // Otherwise, render the TweetAuthorInfo component
-  let tweetAuthorInfo = null
-  if (isNewTweet) {
-    tweetAuthorInfo = (
-      <div className={tweetAuthorInfoClass}></div>
-    )
-  } else {
-    tweetAuthorInfo = (
-      <TweetAuthorInfo tweetObject={tweetObject} />
-    )
-  }      
-
-  // If tweet is new, render the input field
-  // Otherwise, render the tweet text
-  let tweetContent = null
-  if (isNewTweet) {
-    tweetContent = (
-      <div className='TweetContent'>
-        <input className='TweetContent__input' type='text' placeholder="What's happening?" />
-      </div>
-    )
-  } else {
-    tweetContent = (
-      <div className='TweetContent'>
-        <span className='TweetContent__text'>{tweetObject.text}</span>
-      </div>
-    )
-  }
-
-  // ##############################################################
-  const hoursAgo = (() => {
-    if (isNewTweet) return null
-    return Math.floor((new Date() - tweetObject.tweetTime) / 1000 / 60 / 60)
-  })()
-  // ##############################################################
-
-  // If tweet is new, render the tweet button
-  let tweetButtonContainer = null
-  if (isNewTweet) {
-    tweetButtonContainer = (
-      <div className='TweetActions__right'>
-        <button className='TweetActions__right__post'>Tweet</button>
-      </div>
-    )
-  } 
+  const hoursAgo = Math.floor((new Date() - tweetObject.tweetTime) / 1000 / 60 / 60)
 
   return (
-    <div className='TweetRight' id={tweetElID}>
-      {tweetAuthorInfo}
+    <div className='TweetRight' id={`tweet-${tweetObject.id}`}>
+      <div className='TweetAuthorInfo'>
+        <span className='TweetRight__authorInfo__name'>{tweetObject.user.name}</span>
+        <span className='TweetRight__authorInfo__handle'> | @{tweetObject.user.handle}</span>
+        <span className='TweetRight__authorInfo__time'> · {hoursAgo}h</span>
+      </div>
 
       <div className='TweetContent'>
-        {tweetContent}
+        <span className='TweetContent__text'>{tweetObject.text}</span>
       </div>
 
       <div className='TweetActions'>
         <div className='TweetActions__left'>[Placeholder for media]</div>
-        {tweetButtonContainer}
       </div>
     </div>
   )
