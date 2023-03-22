bcc0a9d8f8421bf591d8fa883b5fb0f0b60486ce
Matthew Burns <msb@ameritech.net>
2023-03-22 10:01:24 -0400

    Finish Message UI

--- app/javascript/components/Messages/MessageViewPane.jsx
+++ app/javascript/components/Messages/MessageViewPane.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import profilePrimaryColor from '../../images/profile-primarycolor.svg'
+import sendArrowRight from '../../images/send-arrow-right.svg'
 
 function MessageViewPane({ messageObject }) {
   return (
@@ -21,6 +22,17 @@ function MessageViewPane({ messageObject }) {
           <div className="MessageViewPane__body__message__date">
             {messageObject.date}
           </div>
+        </div>
+      </div>
+
+      {/* Box to send a reply */}
+      <div className="MessageViewPane__footer">
+        <div className="MessageViewPane__replyContainer">
+          <div className="MessageViewPane__replyContainer__replyBox">
+            <input type="text" placeholder="Reply..." className='MessageViewPane__replyContainer__input' />
+          </div>
+          
+          <img src={sendArrowRight} alt='Send Reply' className='MessageViewPane__replyContainer__sendBtn' />
         </div>
       </div>
     </div>
