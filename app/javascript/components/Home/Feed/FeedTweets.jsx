5d7f5f6ad1f297ff6a2eeaec84219a2a3da3b90b
Matthew Burns <msb@ameritech.net>
2023-03-22 16:18:26 -0400

    Start work on Tweet API

--- app/javascript/components/Home/Feed/FeedTweets.jsx
+++ app/javascript/components/Home/Feed/FeedTweets.jsx
@@ -6,6 +6,18 @@ function FeedTweets() {
   const tweetTestText = `
   Bacon ipsum dolor amet short loin pork belly chislic kevin beef ribs ham cupim shankle jerky capicola filet mignon landjaeger cow drumstick. Biltong burgdoggen strip steak sausage kielbasa jerky turkey pancetta porchetta alcatra filet mignon. 
   `
+
+  // Fetch tweets from API at /api/v1/tweets
+  fetch('/api/v1/tweets')
+    .then((response) => response.json())
+    .then((data) => testMethod(data[0]))
+
+
+  function testMethod(myInput) {
+    console.log("The date from Rails API:", myInput.created_at)
+    console.log("Parsed date:", new Date(myInput.created_at))
+  }
+
   const testTweetObject =  {
     id: 1,
     text: tweetTestText,
