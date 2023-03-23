82449079d81b70227d7106fef528e970e3d7a3b3
Matthew Burns <msb@ameritech.net>
2023-03-22 20:07:40 -0400

    Fix bug on tweet integration

--- app/javascript/components/Home/Feed/FeedTweets.jsx
+++ app/javascript/components/Home/Feed/FeedTweets.jsx
@@ -1,51 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import FeedNewTweet from './FeedNewTweet'
 import Tweet from './Tweet'
 
 function FeedTweets() {
-  const tweetTestText = `
-  Bacon ipsum dolor amet short loin pork belly chislic kevin beef ribs ham cupim shankle jerky capicola filet mignon landjaeger cow drumstick. Biltong burgdoggen strip steak sausage kielbasa jerky turkey pancetta porchetta alcatra filet mignon. 
-  `
-
-  // // Fetch tweets from API at /api/v1/tweets
-  // [tweetsArray, setTweetsArray] = useState([])
-  // useEffect(() => {
-  //   fetch('/api/v1/tweets')
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setTweetsArray(data)
-  //     })
-  // }, [])
-
-  // const testTweetObject =  {
-  //   id: 1,
-  //   content: tweetTestText,
-  //   user: {
-  //     id: 1,
-  //     name: 'Matt Burns',
-  //     handle: 'msburns24'
-  //   },
-  //   tweetTime: new Date(2023, 2, 17, 7, 0, 0)
-  // }
-
-  // const [tweets, setTweets] = useState([testTweetObject])
-
   const [users, setUsers] = useState([])
-  fetch('/api/v1/users')
-    .then((response) => response.json())
-    .then((data) => {
-      setUsers(data)
-    })
-
   const [tweets, setTweets] = useState([])
-  fetch('/api/v1/tweets')
-    .then((response) => response.json())
-    .then((data) => {
-      setTweets(data)
-    })
-    .then(
-      console.log(tweets)
-    )
+
+  useEffect(() => {
+    fetch('/api/v1/users')
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data)
+      })
+    
+    fetch('/api/v1/tweets')
+      .then((response) => response.json())
+      .then((data) => {
+        setTweets(data)
+      })
+  }, [])
 
   const addTweet = (newTweet) => {
     setTweets([newTweet, ...tweets])
--- app/javascript/components/Home/Feed/TweetRight.jsx
+++ app/javascript/components/Home/Feed/TweetRight.jsx
@@ -1,19 +1,38 @@
 import React from 'react'
 
-function TweetRight({ isNewTweet, tweetObject, users }) {
-
-  const hoursAgo = Math.floor((new Date() - tweetObject.created_at) / 1000 / 60 / 60)
+function TweetRight({ tweetObject, users }) {
   const user = users.find(user => user.id === tweetObject.user_id)
 
-  console.log(users)
+  let authorName, authorHandle
+  if (user) {
+    authorName = user.name
+    authorHandle = user.username
+  }
+
+  let timestamp
+  const tweetDate = new Date(tweetObject.created_at)
+  const now = new Date()
+  if ((now - tweetDate) < (1000*60)) {
+    timestamp = 'Just now'
+  } else if ((now - tweetDate) < (1000*60*60)) {
+    timestamp = `${Math.floor((now - tweetDate) / (1000*60))}m`
+    
+  } else if ((now - tweetDate) < (1000*60*60*24)) {
+    timestamp = `${Math.floor((now - tweetDate) / (1000*60*60))}h`
+
+  } else if ((now - tweetDate) < (1000*60*60*24*7)) {
+    timestamp = `${Math.floor((now - tweetDate) / (1000*60*60*24))}d`
+
+  } else {
+    timestamp = `${tweetDate.getMonth() + 1}/${tweetDate.getDate()}/${tweetDate.getFullYear()}`
+  }
 
   return (
     <div className='TweetRight' id={`tweet-${tweetObject.id}`}>
       <div className='TweetAuthorInfo'>
-        <span className='TweetRight__authorInfo__name'>{user.name}</span>
-        {/* <span className='TweetRight__authorInfo__handle'> | @{tweetObject.user.handle}</span> */}
-        <span className='TweetRight__authorInfo__handle'> | @{user.username}</span>
-        <span className='TweetRight__authorInfo__time'> · {hoursAgo}h</span>
+        <span className='TweetRight__authorInfo__name'>{authorName}</span>
+        <span className='TweetRight__authorInfo__handle'> | @{authorHandle}</span>
+        <span className='TweetRight__authorInfo__time'> · {timestamp}</span>
       </div>
 
       <div className='TweetContent'>
