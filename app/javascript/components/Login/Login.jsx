6f899996605d31f0800ec244c0901c6af627646e
Matthew Burns <msb@ameritech.net>
2023-03-23 19:35:08 -0400

    Adjust styling on login

--- app/javascript/components/Login/Login.jsx
+++ app/javascript/components/Login/Login.jsx
@@ -8,47 +8,54 @@ function Login() {
     <div className='login-container'>
       <div id='Login'>
         <div className="login-left">
-          <img src={logoWhiteIcon} alt="logo" className="icon-60" />
+          <img src={logoWhiteIcon} alt="logo" className="icon-80" />
         </div>
 
         <div className="login-right">
-          <h1>Login</h1>
+          <div className="container">
+            <h1>Login</h1>
 
-          <form action='/login' method='post'>
-            <div className='form-group'>
-              <label htmlFor='email'>
-                <img src={emailIcon} alt='email' className='icon-40' />
-              </label>
-              <input
-                type='email'
-                name='email'
-                id='email'
-                className='form-control'
-                placeholder='Email'
-              />
-            </div>
-            <div className='form-group'>
-              <label htmlFor='password'>
-                <img src={passwordIcon} alt='email' className='icon-30' />
-              </label>
-              <input
-                type='password'
-                name='password'
-                id='password'
-                className='form-control'
-                placeholder='Password'
-              />
-            </div>
-            <div className='form-actions'>
-              <div className='remember-me'>
-                <input type='checkbox' name='remember_me' id='remember_me' />
-                <label htmlFor='remember_me'>Remember me</label>
+            <form action='/login' method='post'>
+              <div className='form-group'>
+                <label htmlFor='email'>
+                  <img src={emailIcon} alt='email' className='icon-40' />
+                </label>
+                <input
+                  type='email'
+                  name='email'
+                  id='email'
+                  className='form-control'
+                  placeholder='Email'
+                />
               </div>
-              <button type='submit' className='btn btn-primary'>
-                Login
-              </button>
-            </div>
-          </form>
+              <div className='form-group'>
+                <label htmlFor='password'>
+                  <img src={passwordIcon} alt='email' className='icon-30' />
+                </label>
+                <input
+                  type='password'
+                  name='password'
+                  id='password'
+                  className='form-control'
+                  placeholder='Password'
+                />
+              </div>
+
+              <div className="signup">
+                <p>Don't have an account? <a href="/signup" className="link">Sign up</a></p>
+              </div>
+              
+              <div className='form-actions'>
+                <div className='remember-me'>
+                  <input type='checkbox' name='remember_me' id='remember_me' />
+                  <label htmlFor='remember_me'>Remember me</label>
+                </div>
+                <button type='submit' className='btn btn-primary'>
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
