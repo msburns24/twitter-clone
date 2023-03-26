d06b95bf88b97040c591c0639e68e65c72c1e621
Matthew Burns <msb@ameritech.net>
2023-03-26 10:47:39 -0400

    Setup user signup

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
-    registration: 'signup'
+    sign_up: 'signup'
   }
 
   # Handle all others through React
